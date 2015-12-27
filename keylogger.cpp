40543be0dd0c538bd5d41b4ac340547100c8c47f
Thomas Hood <thomashood10@yahoo.com>
2015-12-27 12:16:36 +0000

    keylogger.cpp -- simple cin to text file for fun

---
+++ keylogger.cpp
@@ -0,0 +1,31 @@
+// Keylogger
+
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <Lmcons.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	// Get username
+	LPWSTR username;
+	DWORD username_len = UNLEN + 1;
+	std::string user;
+	user = GetUserName(username, &username_len);
+
+		ofstream log;
+		log.open("C:\\Users\\" + user + "\\Documents\\dxlog.txt");
+		if (log.good()) {
+			log << cin.getline;
+			log.close;
+		}
+		else {
+		// Copy/replicate, send on all interfaces, terminate current process
+			exit(1);
+		}
+}
+
